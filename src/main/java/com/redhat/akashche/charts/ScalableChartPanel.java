43045184b6370e7ebbdaee8b0f32b16e77ce650d
akashche <akashche@redhat.com>
2015-07-29 20:38:19 +0300

    bar listener added

--- src/main/java/com/redhat/akashche/charts/ScalableChartPanel.java
+++ src/main/java/com/redhat/akashche/charts/ScalableChartPanel.java
@@ -1,8 +1,18 @@
 package com.redhat.akashche.charts;
 
+import org.jfree.chart.ChartMouseEvent;
+import org.jfree.chart.ChartMouseListener;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.entity.CategoryItemEntity;
+import org.jfree.chart.entity.ChartEntity;
+import org.jfree.chart.entity.XYItemEntity;
+import org.jfree.data.xy.XYDataItem;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
+import javax.swing.*;
 import java.awt.*;
 
 import static com.redhat.akashche.charts.ChartBuilder.toColor;
@@ -20,5 +30,27 @@ public class ScalableChartPanel extends ChartPanel {
         this.setRangeZoomable(false);
 //        this.setZoomOutlinePaint(Color.blue);
         this.setZoomFillPaint(toColor("#55ff9800"));
+        this.addChartMouseListener(new RightClickListener());
+    }
+
+    private class RightClickListener implements ChartMouseListener {
+        @Override
+        public void chartMouseClicked(ChartMouseEvent event) {
+            ChartEntity entity = event.getEntity();
+            if (entity instanceof CategoryItemEntity) {
+                CategoryItemEntity en = (CategoryItemEntity) entity;
+                System.out.println(en);
+
+                JPopupMenu menu = new JPopupMenu();
+                menu.add(new JLabel("Value: " + en.getRowKey().toString()));
+
+                menu.show(ScalableChartPanel.this, event.getTrigger().getX(), event.getTrigger().getY());
+
+            }
+        }
+
+        @Override
+        public void chartMouseMoved(ChartMouseEvent event) {
+        }
     }
 }
